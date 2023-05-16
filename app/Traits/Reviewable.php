c2466d07bf28663d3ae3fb06bf45833cc038231f
Mahmud <mamatovmahmud1999@gmail.com>
2023-05-16 16:01:27 +0500

    bug fix on review

--- app/Traits/Reviewable.php
+++ app/Traits/Reviewable.php
@@ -8,21 +8,17 @@ trait Reviewable
 {
     public function addReview($params)
     {
-        $review = new Review();
-        $review->user_id = auth('sanctum')->id();
-        $review->rating = $params->rating;
-        $review->comment = $params->comment ?? null;
 
-        $this->reviews()->updateOrCreate([
+        $review = $this->reviews()->updateOrCreate([
             'user_id' => auth('sanctum')->id(),
             'reviewable_id' => $this->id,
             'reviewable_type' => self::class,
-        ], [
+        ],[
             'rating' => $params->rating,
             'comment' => $params->comment,
         ]);
 
-        if (isset($params->images) && count($params->images) > 0) {
+        if (isset($params->images) && count($params->images) > 0){
             $review->uploads($params->images);
             $review->update(['img' => $params->images[0]]);
         }
