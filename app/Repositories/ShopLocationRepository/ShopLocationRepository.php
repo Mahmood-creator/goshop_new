02a0b62842abbe4d8b05ae270af9d6261157e9a2
Mahmud <mamatovmahmud1999@gmail.com>
2023-05-24 12:16:43 +0500

    add changes

--- app/Repositories/ShopLocationRepository/ShopLocationRepository.php
+++ app/Repositories/ShopLocationRepository/ShopLocationRepository.php
@@ -31,11 +31,10 @@ class ShopLocationRepository extends CoreRepository
 
     public function show(int $id, int $shop = null)
     {
-        return $this->model()->with(['country:id,name', 'region:name', 'city:name'])
+        return $this->model()->with(['country', 'region', 'city'])
             ->when(isset($shop), function ($q) use ($shop) {
                 $q->where('shop_id', $shop);
             })
-            ->select('id','shop_id','delivery_fee','pickup','country_id','region_id','city_id')
             ->find($id);
     }
 }
