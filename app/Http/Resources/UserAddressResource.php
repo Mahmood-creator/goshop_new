34a3fd198ac0b49f2d00ad28e6173bd04f5537e2
Mahmud <mamatovmahmud1999@gmail.com>
2023-06-07 19:15:01 +0500

    add changes

--- app/Http/Resources/UserAddressResource.php
+++ app/Http/Resources/UserAddressResource.php
@@ -24,8 +24,8 @@ class UserAddressResource extends JsonResource
             'country_id' => $this->country_id,
             'note' => $this->note,
             'location' => [
-                'latitude' => $this->location['latitude'],
-                'longitude' => $this->location['longitude'],
+                'latitude' => (double) $this->location['latitude'],
+                'longitude' => (double) $this->location['longitude'],
             ],
             'default' => (boolean) $this->default,
             'active' => (boolean) $this->active,
